57e02999acec11c5964fc9dfea6e6bc4dc5177b5
tjdgusdk <sha6639@naver.com>
2025-01-14 15:38:25 +0900

    feat: Question Entity 추가

---
+++ lms/src/main/java/com/example/lms/domain/question/entity/Question.java
@@ -0,0 +1,38 @@
+package com.example.lms.domain.question.entity;
+
+import com.example.lms.common.base.BaseTimeEntity;
+import com.example.lms.domain.answer.entity.Answer;
+import com.example.lms.domain.quiz.entity.Quiz;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@NoArgsConstructor
+@Entity
+@Table(name = "question")
+public class Question extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long questionId;
+
+    @ManyToOne
+    @JoinColumn(name = "quiz_id", nullable = false)
+    private Quiz quiz;
+
+    @Column(nullable = false, length = 200)
+    private String content;
+
+    @Column(nullable = false, length = 200)
+    private String correct;
+
+    @Column(nullable = false)
+    private Integer point;
+
+    @OneToMany(mappedBy = "question", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Answer> answers = new ArrayList<>();
+}
