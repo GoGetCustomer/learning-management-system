07180e2b066f78e34747db1fae6cde0c0da2ddaa
Hm-source <swo8852@gmail.com>
2025-01-14 16:45:51 +0900

    feat: CourseRepository 생성 #1

---
+++ lms/src/main/java/com/example/lms/domain/course/respository/CourseRepository.java
@@ -0,0 +1,9 @@
+package com.example.lms.domain.course.respository;
+
+import com.example.lms.domain.course.entity.Course;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CourseRepository extends JpaRepository<Course, Long> {
+}
