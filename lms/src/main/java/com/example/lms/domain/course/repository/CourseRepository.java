13f26bf0d805e0f5601e35b6ce743ba445701df6
Hm-source <swo8852@gmail.com>
2025-01-15 14:58:11 +0900

    rename: 패키지 명 오타 수정.

--- lms/src/main/java/com/example/lms/domain/course/repository/CourseRepository.java
+++ lms/src/main/java/com/example/lms/domain/course/repository/CourseRepository.java
@@ -1,9 +1,12 @@
-package com.example.lms.domain.course.respository;
+package com.example.lms.domain.course.repository;
 
 import com.example.lms.domain.course.entity.Course;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface CourseRepository extends JpaRepository<Course, Long> {
+    List<Course> findByInstructor_Id(Long instructorId);
 }
