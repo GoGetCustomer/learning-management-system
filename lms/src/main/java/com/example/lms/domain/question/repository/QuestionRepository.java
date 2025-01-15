fb08a80bc3df5ae87d72affee2b7fb17e3d9a874
tjdgusdk <sha6639@naver.com>
2025-01-15 16:53:23 +0900

    fix: Repository 애너테이션 삭제

--- lms/src/main/java/com/example/lms/domain/question/repository/QuestionRepository.java
+++ lms/src/main/java/com/example/lms/domain/question/repository/QuestionRepository.java
@@ -4,6 +4,5 @@ import com.example.lms.domain.question.entity.Question;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
 public interface QuestionRepository extends JpaRepository<Question, Long> {
 }
