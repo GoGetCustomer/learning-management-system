c4e5ea64e7406fa9939aef034fd7722e77533cbd
Hm-source <swo8852@gmail.com>
2025-01-15 14:56:26 +0900

    refactor: Course entity에서 instructor/content/lecture 연결관계 설정 및 정적 팩토리 메서드 적용 #1

--- lms/src/main/java/com/example/lms/domain/course/entity/Course.java
+++ lms/src/main/java/com/example/lms/domain/course/entity/Course.java
@@ -1,20 +1,22 @@
 package com.example.lms.domain.course.entity;
 
 import com.example.lms.common.base.BaseTimeEntity;
+import com.example.lms.domain.content.entity.Content;
+import com.example.lms.domain.instructor.entity.Instructor;
+import com.example.lms.domain.lecture.entity.Lecture;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name="course")
 @Getter
 @NoArgsConstructor
-@AllArgsConstructor
-@Builder
 public class Course extends BaseTimeEntity {
 
     @Id
@@ -35,5 +37,45 @@ public class Course extends BaseTimeEntity {
     private LocalDate endDate;
 
     @Column(name = "course_capacity", nullable = false)
-    private int courseCapacity;
+    private Integer courseCapacity;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "instructor_id", nullable = false)
+    private Instructor instructor;
+
+    @OneToMany(mappedBy = "course", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Lecture> lectures = new ArrayList<>();
+
+    @OneToMany(mappedBy = "course", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Content> contents = new ArrayList<>();
+
+
+    /**
+     * 등록 정보 (OneToMany 관계)
+     *
+     * @OneToMany(mappedBy = "course", cascade = CascadeType.ALL, orphanRemoval = true)
+     * private List<Registration> registrations = new ArrayList<>();
+     */
+
+    @Builder
+    public Course(Long courseId, String courseTitle, String courseDescription, LocalDate startDate, LocalDate endDate, int courseCapacity, Instructor instructor) {
+        this.courseId = courseId;
+        this.courseTitle = courseTitle;
+        this.courseDescription = courseDescription;
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.courseCapacity = courseCapacity;
+        this.instructor = instructor;
+    }
+
+    public static Course of(String courseTitle, String courseDescription, LocalDate startDate, LocalDate endDate, int courseCapacity, Instructor instructor) {
+        return Course.builder()
+                .courseTitle(courseTitle)
+                .courseDescription(courseDescription)
+                .startDate(startDate)
+                .endDate(endDate)
+                .courseCapacity(courseCapacity)
+                .instructor(instructor)
+                .build();
+    }
 }
