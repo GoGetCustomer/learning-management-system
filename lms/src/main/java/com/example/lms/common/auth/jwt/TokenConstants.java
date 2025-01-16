e5f97a4bc13fe3a01944b5fe16fecfb9d65bc2c4
조영무 <fprh13@gmail.com>
2025-01-16 17:35:47 +0900

    feat: 토큰 관련 상수 통합 관리 #5

---
+++ lms/src/main/java/com/example/lms/common/auth/jwt/TokenConstants.java
@@ -0,0 +1,10 @@
+package com.example.lms.common.auth.jwt;
+
+public class TokenConstants {
+    public static final String AUTHORITIES_KEY = "roles";
+    public static final String REDIS_PREFIX_REFRESH = "RT:";
+
+    public static final String AUTHORIZATION_HEADER = "Authorization";
+    public static final String BEARER_PREFIX = "Bearer ";
+    public static final String REFRESH_TOKEN_COOKIE_NAME = "refresh_token";
+}
