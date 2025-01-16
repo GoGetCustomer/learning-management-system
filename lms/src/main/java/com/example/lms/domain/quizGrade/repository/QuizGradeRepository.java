fc0896840eb90917500f0b23b5b67e107654411b
tjdgusdk <sha6639@naver.com>
2025-01-16 09:43:28 +0900

    feat: QuizGrade Repository 추가

---
+++ lms/src/main/java/com/example/lms/domain/quizGrade/repository/QuizGradeRepository.java
@@ -0,0 +1,7 @@
+package com.example.lms.domain.quizGrade.repository;
+
+import com.example.lms.domain.quizGrade.entity.QuizGrade;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface QuizGradeRepository extends JpaRepository<QuizGrade, Long> {
+}
