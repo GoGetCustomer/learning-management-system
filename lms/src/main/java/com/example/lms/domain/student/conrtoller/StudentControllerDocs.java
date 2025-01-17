bfb8a300833fd2437761f42fab65ad78f2bb23b5
조영무 <fprh13@gmail.com>
2025-01-17 14:24:33 +0900

    feat: 학생, 강사 회원가입 및 로그인 API 작성 및 문서화 #5

---
+++ lms/src/main/java/com/example/lms/domain/student/conrtoller/StudentControllerDocs.java
@@ -0,0 +1,30 @@
+package com.example.lms.domain.student.conrtoller;
+
+import com.example.lms.common.auth.dto.LoginRequestDto;
+import com.example.lms.domain.student.dto.StudentCreateRequestDto;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+
+@Tag(name = "Students")
+@Validated
+public interface StudentControllerDocs {
+
+    @Operation(summary = "학생 로그인 요청", description = "학생으로 로그인을 진행하면 헤더 accessToken, 쿠키 refreshToken이 발급 됩니다.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "학생 로그인 성공"),
+            @ApiResponse(responseCode = "403", description = "권한 없습니다.")
+    })
+    public ResponseEntity<?> login(LoginRequestDto loginRequestDto);
+
+    @Operation(summary = "학생 회원가입 요청", description = "**성공 응답 데이터:** 학생의 `student_id` ")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "학생 회원 가입 완료"),
+            @ApiResponse(responseCode = "400", description = "잘못된 입력 데이터"),
+            @ApiResponse(responseCode = "409", description = "아이디/이메일 중복")
+    })
+    public ResponseEntity<Long> join (StudentCreateRequestDto studentCreateRequestDto);
+}
