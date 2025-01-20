e37004abbd438a1b6405ae9696405f625f7a48d4
tjdgusdk <sha6639@naver.com>
2025-01-20 13:24:14 +0900

    fix: 엔티티 id 관련 수정 및 연관관계 매핑 #2

--- lms/src/main/java/com/example/lms/domain/quiz/repository/QuizRepository.java
+++ lms/src/main/java/com/example/lms/domain/quiz/repository/QuizRepository.java
@@ -2,12 +2,10 @@ package com.example.lms.domain.quiz.repository;
 
 import com.example.lms.domain.quiz.entity.Quiz;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface QuizRepository extends JpaRepository<Quiz, Long> {
 
-    List<Quiz> findByCourseId(Long courseId);
+    List<Quiz> findByCourse_CourseId(Long courseId);
 }
