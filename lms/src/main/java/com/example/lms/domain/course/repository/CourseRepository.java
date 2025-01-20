6f2c5a735765042cab2e6d5c6b313b98f167d1bf
Hm-source <swo8852@gmail.com>
2025-01-20 16:31:22 +0900

    feat: courseId 별로 단일 과정 조회 메서드 구현 및 강사 Id 별로 과정 리스트 반환 & 전체 반환 메서드 구현

--- lms/src/main/java/com/example/lms/domain/course/repository/CourseRepository.java
+++ lms/src/main/java/com/example/lms/domain/course/repository/CourseRepository.java
@@ -2,7 +2,12 @@ package com.example.lms.domain.course.repository;
 
 import com.example.lms.domain.course.entity.Course;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 
 public interface CourseRepository extends JpaRepository<Course, Long> {
+    @Query("SELECT DISTINCT c FROM Course c JOIN FETCH c.teachings t WHERE t.instructor.id = :instructorId")
+    List<Course> findAllByInstructorId(Long instructorId);
 }
--- lms/src/main/java/com/example/lms/domain/course/service/CourseService.java
+++ lms/src/main/java/com/example/lms/domain/course/service/CourseService.java
@@ -3,10 +3,12 @@ package com.example.lms.domain.course.service;
 import com.example.lms.domain.course.dto.request.CourseCreateRequestDto;
 import com.example.lms.domain.course.dto.request.CourseUpdateRequestDto;
 import com.example.lms.domain.course.dto.response.CourseCreateResponseDto;
+import com.example.lms.domain.course.dto.response.CourseResponseDto;
 import com.example.lms.domain.course.dto.response.CourseUpdateResponseDto;
 import com.example.lms.domain.course.entity.Course;
 import com.example.lms.domain.course.mapper.CourseMapper;
 import com.example.lms.domain.course.repository.CourseRepository;
+import com.example.lms.domain.instructor.dto.InstructorInfo;
 import com.example.lms.domain.instructor.entity.Instructor;
 import com.example.lms.domain.instructor.repository.InstructorRepository;
 import com.example.lms.domain.teaching.entity.Teaching;
@@ -15,6 +17,9 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 
 @RequiredArgsConstructor
@@ -82,6 +87,63 @@ public class CourseService {
         }
         course.getTeachings().clear();
         courseRepository.delete(course);
+    }
+
+    @Transactional(readOnly = true)
+    public CourseResponseDto getCourseById(Long courseId) {
+        // courseId로 Course 조회
+        Course course = courseRepository.findById(courseId)
+                .orElseThrow(() -> new IllegalArgumentException("Course not found with ID: " + courseId));
+
+        InstructorInfo instructorInfo = course.getTeachings().stream()
+                .findFirst()
+                .map(teaching -> InstructorInfo.builder()
+                        .instructorId(teaching.getInstructor().getId())
+                        .name(teaching.getInstructor().getName())
+                        .email(teaching.getInstructor().getEmail())
+                        .description(teaching.getInstructor().getDescription())
+                        .build())
+                .orElse(null);
+
+        return courseMapper.toResponseDto(course, instructorInfo);
+    }
+
+    @Transactional(readOnly = true)
+    public List<CourseResponseDto> getCourseByInstructorId(Long instructorId) {
+        List<Course> courses;
+        if (instructorId == null) {
+            courses = courseRepository.findAll();
+        } else {
+            Instructor instructor = instructorRepository.findById(instructorId)
+                    .orElseThrow(() -> new IllegalArgumentException("Instructor not found with ID: " + instructorId));
+
+            courses = courseRepository.findAllByInstructorId(instructorId);
+
+            if (courses.isEmpty()) {
+                throw new IllegalArgumentException("해당 강사가 담당하는 과정이 없습니다.");
+            }
+        }
+
+        return courses.stream()
+                .map(course -> {
+                    Instructor instructor = course.getTeachings()
+                            .stream()
+                            .findFirst() // 강의는 하나의 강사에게만 귀속된다는 가정
+                            .map(Teaching::getInstructor)
+                            .orElse(null);
+
+                    InstructorInfo instructorInfo = instructor != null
+                            ? InstructorInfo.builder()
+                            .instructorId(instructor.getId())
+                            .name(instructor.getName())
+                            .email(instructor.getEmail())
+                            .description(instructor.getDescription())
+                            .build()
+                            : null;
+
+                    return courseMapper.toResponseDto(course, instructorInfo);
+                })
+                .collect(Collectors.toList());
     }
 
 }
