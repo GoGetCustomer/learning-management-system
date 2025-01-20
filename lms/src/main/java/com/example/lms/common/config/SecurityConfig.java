86e46d5a548d03ac4c92240e5ebe417027f70d12
tjdgusdk <sha6639@naver.com>
2025-01-20 14:12:20 +0900

    feat: Security 에러 핸들링 추가 #5

--- lms/src/main/java/com/example/lms/common/config/SecurityConfig.java
+++ lms/src/main/java/com/example/lms/common/config/SecurityConfig.java
@@ -1,28 +1,101 @@
 package com.example.lms.common.config;
 
+import com.example.lms.common.auth.filter.*;
+import com.example.lms.common.auth.handler.CustomAccessDeniedHandler;
+import com.example.lms.common.auth.handler.CustomAuthenticationEntryPoint;
+import com.example.lms.common.auth.jwt.TokenProvider;
+import com.example.lms.common.auth.service.CustomInstructorDetailsService;
+import com.example.lms.common.auth.service.CustomStudentDetailsService;
+import com.example.lms.domain.user.enums.Role;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.authentication.logout.LogoutFilter;
+
+import java.util.List;
 
 @Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+	private final TokenProvider tokenProvider;
+	private final ObjectMapper objectMapper;
+
 	@Bean
-	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-		return http
-			.csrf(AbstractHttpConfigurer::disable)
-			.authorizeHttpRequests(auth -> auth
-				.requestMatchers(SWAGGER_PATTERNS).permitAll()
-				.anyRequest().authenticated()
-			)
-			.build();
+	public SecurityFilterChain securityFilterChain(HttpSecurity http,
+												   @Qualifier("studentAuthenticationManager") AuthenticationManager studentAuthenticationManager,
+												   @Qualifier("instructorAuthenticationManager") AuthenticationManager instructorAuthenticationManager) throws Exception {
+		http
+				.csrf(AbstractHttpConfigurer::disable)
+				.formLogin(AbstractHttpConfigurer::disable)
+				.httpBasic(AbstractHttpConfigurer::disable)
+				.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+				.authorizeHttpRequests(auth -> auth
+						.requestMatchers(SWAGGER_PATTERNS).permitAll()
+						.requestMatchers(HttpMethod.POST, "/api/students/signup", "/api/instructors/signup").permitAll()
+						.requestMatchers(HttpMethod.GET, "/api/users/reissue").permitAll()
+						.requestMatchers("/api/students/**").hasAuthority(Role.STUDENT.getAuthority())
+						.requestMatchers("/api/instructors/**").hasAuthority(Role.INSTRUCTOR.getAuthority())
+						.anyRequest().authenticated()
+				)
+				.addFilterAt(new StudentLoginFilter(studentAuthenticationManager, tokenProvider, objectMapper), UsernamePasswordAuthenticationFilter.class)
+				.addFilterAt(new InstructorLoginFilter(instructorAuthenticationManager, tokenProvider, objectMapper), UsernamePasswordAuthenticationFilter.class)
+				.addFilterAfter(new JwtAuthenticationFilter(tokenProvider), CustomUsernamePasswordAuthenticationFilter.class)
+				.addFilterBefore(new CustomLogoutFilter(tokenProvider, objectMapper), LogoutFilter.class)
+				.exceptionHandling((exceptionHandling) -> exceptionHandling
+						.authenticationEntryPoint(new CustomAuthenticationEntryPoint(objectMapper))
+						.accessDeniedHandler(new CustomAccessDeniedHandler(objectMapper)));
+		return http.build();
+	}
+
+	@Bean
+	@Primary
+	public AuthenticationManager studentAuthenticationManager(
+			CustomStudentDetailsService studentDetailsService,
+			BCryptPasswordEncoder bCryptPasswordEncoder
+	) {
+		DaoAuthenticationProvider studentAuthProvider = new DaoAuthenticationProvider();
+		studentAuthProvider.setUserDetailsService(studentDetailsService);
+		studentAuthProvider.setPasswordEncoder(bCryptPasswordEncoder);
+
+		return new ProviderManager(List.of(studentAuthProvider));
+	}
+
+	@Bean
+	public AuthenticationManager instructorAuthenticationManager(
+			CustomInstructorDetailsService instructorDetailsService,
+			BCryptPasswordEncoder bCryptPasswordEncoder
+	) {
+		DaoAuthenticationProvider instructorAuthProvider = new DaoAuthenticationProvider();
+		instructorAuthProvider.setUserDetailsService(instructorDetailsService);
+		instructorAuthProvider.setPasswordEncoder(bCryptPasswordEncoder);
+
+		return new ProviderManager(List.of(instructorAuthProvider));
+	}
+
+	@Bean
+	public BCryptPasswordEncoder bCryptPasswordEncoder() {
+		return new BCryptPasswordEncoder();
 	}
 
 	private static final String[] SWAGGER_PATTERNS = {
-		"/swagger-ui/**",
-		"/actuator/**",
-		"/v3/api-docs/**",
+			"/swagger-ui/**",
+			"/actuator/**",
+			"/v3/api-docs/**",
 	};
-
 }
