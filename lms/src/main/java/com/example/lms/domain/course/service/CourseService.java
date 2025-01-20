039d7517a2cd1c4d2e4d246ab9d81d6ef021864b
Hm-source <swo8852@gmail.com>
2025-01-20 13:58:36 +0900

    refactor: 서비스 레이어 내 과정 수정 로직 작성.

--- lms/src/main/java/com/example/lms/domain/course/service/CourseService.java
+++ lms/src/main/java/com/example/lms/domain/course/service/CourseService.java
@@ -1,7 +1,9 @@
 package com.example.lms.domain.course.service;
 
 import com.example.lms.domain.course.dto.request.CourseCreateRequestDto;
+import com.example.lms.domain.course.dto.request.CourseUpdateRequestDto;
 import com.example.lms.domain.course.dto.response.CourseCreateResponseDto;
+import com.example.lms.domain.course.dto.response.CourseUpdateResponseDto;
 import com.example.lms.domain.course.entity.Course;
 import com.example.lms.domain.course.mapper.CourseMapper;
 import com.example.lms.domain.course.repository.CourseRepository;
@@ -39,6 +41,29 @@ public class CourseService {
         Course savedCourse = courseRepository.save(course);
 
         return courseMapper.toCreateResponseDto(savedCourse);
+    }
+    @Transactional
+    public CourseUpdateResponseDto updateCourse(Long courseId, CourseUpdateRequestDto requestDto) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String loginId = authentication.getName();
+
+        Instructor instructor = instructorRepository.findByLoginIdAndNotDeleted(loginId)
+                .orElseThrow(() -> new IllegalArgumentException("로그인된 사용자가 강사가 아닙니다."));
+
+        Course course = courseRepository.findById(courseId)
+                .orElseThrow(() -> new IllegalArgumentException("해당 강좌가 존재하지 않습니다."));
+
+        // 강좌의 소유권 확인
+        if (!course.belongsToInstructor(instructor)) {
+            throw new IllegalArgumentException("해당 강좌를 수정할 권한이 없습니다.");
+        }
+
+        courseMapper.updateEntityFromDto(requestDto, course);
+
+        // 업데이트된 강좌 저장
+        Course savedCourse = courseRepository.save(course);
+
+        return courseMapper.toUpdateResponseDto(savedCourse);
     }
 
 }
