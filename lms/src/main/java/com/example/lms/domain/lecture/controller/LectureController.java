cf26d7db3f77e420b445bf94552d937bff754e80
Hm-source <swo8852@gmail.com>
2025-01-22 00:17:01 +0900

    chore: swagger 설정

--- lms/src/main/java/com/example/lms/domain/lecture/controller/LectureController.java
+++ lms/src/main/java/com/example/lms/domain/lecture/controller/LectureController.java
@@ -2,7 +2,13 @@ package com.example.lms.domain.lecture.controller;
 
 import com.example.lms.domain.lecture.dto.request.LectureCreateRequestDto;
 import com.example.lms.domain.lecture.dto.response.LectureCreateResponseDto;
+import com.example.lms.domain.lecture.entity.Lecture;
 import com.example.lms.domain.lecture.service.LectureService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 
 import org.springframework.http.HttpStatus;
@@ -19,6 +25,16 @@ public class LectureController {
     private final LectureService lectureService;
 
     @PostMapping(consumes = {"application/octet-stream", "multipart/form-data"})
+    @Operation(summary = "과정 내 강의 생성", description = "새로운 강의를 업로드합니다.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "강의 업로드 성공",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = Lecture.class))),
+            @ApiResponse(responseCode = "400", description = "Invalid input",
+                    content = @Content),
+            @ApiResponse(responseCode = "500", description = "Internal server error",
+                    content = @Content)
+    })
     public ResponseEntity<LectureCreateResponseDto> createLecture(
             @PathVariable Long courseId,
             @RequestPart("data") LectureCreateRequestDto request,
--- lms/src/main/java/com/example/lms/domain/lecture/dto/request/LectureCreateRequestDto.java
+++ lms/src/main/java/com/example/lms/domain/lecture/dto/request/LectureCreateRequestDto.java
@@ -1,5 +1,6 @@
 package com.example.lms.domain.lecture.dto.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -12,9 +13,11 @@ import lombok.NoArgsConstructor;
 @Builder
 public class LectureCreateRequestDto {
     @NotBlank(message = "강의 제목은 필수 입력 값입니다.")
+    @Schema(description = "업로드하는 강의 제목을 입력해주세요.", example = "스프링 부트 강의 part1")
     private String lectureTitle;
 
     @NotBlank(message = "강의 설명은 필수 입력 값입니다.")
+    @Schema(description = "업로드하는 강의 설명을 입력해주세요.", example = "스프링 부트 과정에서 첫걸음을 내딛는 강의입니다! 편안하게 들어주세요.")
     private String lectureDescription;
 
 }
