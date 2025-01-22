84ec9d62d5f392b09c012d7ea241acbf88907d69
Hm-source <swo8852@gmail.com>
2025-01-22 15:10:02 +0900

    feat: 강의자료 업로드 및 삭제 API 구현

---
+++ lms/src/main/java/com/example/lms/domain/content/controller/ContentController.java
@@ -0,0 +1,60 @@
+package com.example.lms.domain.content.controller;
+
+import com.example.lms.domain.content.dto.response.ContentResponseDto;
+import com.example.lms.domain.content.service.ContentService;
+import com.example.lms.domain.lecture.entity.Lecture;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
+@RestController
+@RequestMapping("/api/course/{courseId}/content")
+public class ContentController {
+
+    private final ContentService contentService;
+
+    public ContentController(ContentService contentService) {
+        this.contentService = contentService;
+    }
+
+    @PostMapping(consumes = {"multipart/form-data"})
+    @Operation(summary = "과정 내 강의자료 생성", description = "새로운 강의자료를 업로드합니다.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "강의 업로드 성공",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = Lecture.class))),
+            @ApiResponse(responseCode = "400", description = "Invalid input",
+                    content = @Content),
+            @ApiResponse(responseCode = "500", description = "Internal server error",
+                    content = @Content)
+    })
+    public ResponseEntity<ContentResponseDto> createContent(
+            @PathVariable Long courseId,
+            @RequestPart("file") MultipartFile file) throws IOException {
+            ContentResponseDto response = contentService.addContent(courseId, file);
+            return ResponseEntity.ok(response);
+    }
+
+    @DeleteMapping("/{contentId}")
+    @Operation(
+            summary = "강의자료 삭제",
+            description = "업로드된 강의자료를 삭제합니다."
+    )
+    public ResponseEntity<String> deleteContent(
+            @Parameter(description = "삭제하려는 강의자료 ID", required = true)
+            @PathVariable Long contentId,
+            @Parameter(description = "강의가 존재하는 과정 ID", required = true)
+            @PathVariable Long courseId) {
+        contentService.deleteContent(courseId, contentId);
+        return ResponseEntity.ok("강의자료가 삭제되었습니다.");
+    }
+
+}
