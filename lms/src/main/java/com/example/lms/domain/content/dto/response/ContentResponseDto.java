15c69fab5744e8f2dfd01024ce911c2ebc34d2e8
Hm-source <swo8852@gmail.com>
2025-01-22 14:58:00 +0900

    feat: ContentResponseDto 생성

---
+++ lms/src/main/java/com/example/lms/domain/content/dto/response/ContentResponseDto.java
@@ -0,0 +1,17 @@
+package com.example.lms.domain.content.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ContentResponseDto {
+    private String fileName;
+    private String fileType;
+    private String fileUrl;
+    private Long courseId;
+}
